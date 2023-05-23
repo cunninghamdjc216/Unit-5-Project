daaa49b9d8b9663282bf16915667baf6176a34d8
cunninghamdjc216 <131170215+cunninghamdjc216@users.noreply.github.com>
2023-05-23 13:35:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let questionOne = document.querySelector(".QuestionOne").value;
+let questionTwo = document.querySelector(".QuestionTwo").value;
+let completedButton = document.querySelector(".completedButton");
+let completedButtonTwo = document.querySelector(".completedButtonTwo");
+
